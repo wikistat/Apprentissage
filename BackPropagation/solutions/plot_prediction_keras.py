ff13aa17eb1f7f6e23466e9b29d86dd8a9c9dbc1
Beatrice Bonneau <laurentb@insa-12621.insa-toulouse.fr>
2023-04-13 16:16:28 +0200

    MAJ TP backpropagation

--- BackPropagation/solutions/plot_prediction_keras.py
+++ BackPropagation/solutions/plot_prediction_keras.py
@@ -1,7 +1,7 @@
 sample_idx = 42
 fig = plt.figure(figsize=(10,4))
 ax = fig.add_subplot(1,2,1)
-ax.imshow(scaler.inverse_transform(X_test[sample_idx]).reshape(8, 8),
+ax.imshow(scaler.inverse_transform(X_test[sample_idx].reshape(1,-1)).reshape(8, 8),
            cmap=plt.cm.gray_r, interpolation='nearest')
 ax.set_title("true label: %d" %y_test[sample_idx])
 ax.axis("off")
@@ -9,7 +9,7 @@ ax.grid(False)
 
 ax1 = fig.add_subplot(1,2,2)
 classes = np.arange(10)
-probabilities = keras_model.predict_proba(X_test, verbose=0)
+probabilities = keras_model.predict(X_test, verbose=0)
 ax1.bar(classes, one_hot(len(classes), y_test[sample_idx]), label='true')
 ax1.bar(classes, probabilities[sample_idx], label='prediction', color="red")
 ax1.set_xticks(classes)
